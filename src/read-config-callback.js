fc2c62309925b70a03478473ad9a268c0031fa55
Mario Yepes <marioy47@gmail.com>
2021-07-24 15:26:17 -0500

    Adding a callbacks to the read configuration function

---
+++ src/read-config-callback.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const path = require("path");
+
+/**
+ * @param {string} filename - File name in the `config/` directory
+ * @param {Function} callback - Function to execute when the config is read
+ * @param {Function} error - Function to execute if an error ocurrs
+ * @returns {string} - The file contents
+ * @throws {Error} - If the file could not be find or opened
+ */
+function readConfig(filename, callback, error) {
+  const config = path.dirname(__dirname) + `/config/${filename}`;
+
+  fs.readFile(`${config}`, "utf8", (err, data) => {
+    if (err) {
+      error(err);
+    }
+    callback(data.trim());
+  });
+}
+
+readConfig(
+  "photos.txt",
+  (contents) => {
+    console.log(`The config contents are "${contents}"`);
+  },
+  (err) => {
+    console.error(`The configuration file could not be read:`, err);
+  }
+);
